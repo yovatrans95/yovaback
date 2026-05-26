5c3e0f3889035628a644e62b7cee0eadb64a3658
yovatrans95 <yovatrans95@gmail.com>
2026-05-26 09:56:24 +0200

    Premier commit

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5500",
+    "http://localhost:5501" ,
     "http://127.0.0.1:5500",
     "https://front-j4x8.onrender.com",
     "https://admin.yovatrans.fr"
