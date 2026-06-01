3165349dc1d1fb267f4fabaf0f294752fa7fe151
yovatrans95 <yovatrans95@gmail.com>
2026-06-01 09:10:40 +0200

    Premier commited

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function generateToken(payload) {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d'
+    expiresIn: process.env.JWT_EXPIRES_IN || '30d'
   });
 }
 
